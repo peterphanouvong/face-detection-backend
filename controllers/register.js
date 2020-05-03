121fcbbd5083378ee182f07a5c09e6796fd9e6ff
peterphanouvong <peterphanouvong@gmail.com>
2020-05-03 20:07:41 +1000

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -23,7 +23,6 @@ const handleRegister = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
         })
-        
     .catch(err => res.status(400).json("unable to register"))
 
 };
--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const root = require('./controllers/root');
 
 const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: true,
     }
 });
 
