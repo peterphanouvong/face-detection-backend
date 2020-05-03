62927f9c5e16d86b1565ae3c2ba6eb4f6288b823
peterphanouvong <peterphanouvong@gmail.com>
2020-05-03 19:37:19 +1000

    homepage

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => root.getUsers(req, res, db));
+app.get('/', (req, res) => res.json('it is working'));
 
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt));
 
